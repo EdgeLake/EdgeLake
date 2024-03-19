ca5ed789042f1f2da869f30ba9c8d97c8daa49a1
Ori Shadmon <oshadmon@gmail.com>
2024-03-19 12:57:38 -0700

    (directions for) migration of AnyLog-Network to EdgeLake
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

---
+++ update_code.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# copy anylog-network/anylog_node into EdgeLake
+for file_path in edge_lake/*/*.py edge_lake/edge_lake.py ; do
+  sed -i '' 's/AnyLog-Network/EdgeLake/g' ${file_path}
+  sed -i '' "s/AL >/EL >/g" ${file_path}
+  sed -i '' 's/anylog_node/edge_lake/g' ${file_path}
+  sed -i '' 's/anylog_path/edgelake_path/g' ${file_path}
+  sed -i '' 's/ANYLOG_LIB/EDGELAKE_LIB/g' ${file_path}
+  sed -i '' 's/ANYLOG_HOME/EDGELAKE_HOME/g' ${file_path}
+done
