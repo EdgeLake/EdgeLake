4cc819b92a636a600c294d183a1e49eabc0e15a8
Ori Shadmon <oshadmon@gmail.com>
2024-03-19 22:10:39 -0700

    update code + readme w/o publishwr
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- other/update_code.sh
+++ other/update_code.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # copy anylog-network/anylog_node into EdgeLake
+cp -r ~/AnyLog-Network/anylog_node/* edge_lake/
+mv edge_lake/anylog.py edge_lake/edgelake.py
 for file_path in edge_lake/*/*.py edge_lake/edge_lake.py ; do
   sed -i '' 's/AnyLog-Network/EdgeLake/g' ${file_path}
   sed -i '' "s/AL >/EL >/g" ${file_path}
